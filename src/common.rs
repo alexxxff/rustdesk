3f49038813154c153f1f387201f5b054f9a9fbc7
alexxxff <248533696@qq.com>
2025-05-26 12:03:15 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://1.95.223.127:21114".to_owned()
 }
 
 #[inline]
